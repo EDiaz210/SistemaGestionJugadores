ae53be17d2882ab932444a769e38a07cc62a742c
Luxyz <elkin.diaz@epn.edu.ec>
2025-01-02 20:28:35 -0500

    Initial commit

---
+++ src/Opciones.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Menu {
+    public JPanel menu;
+    private JButton agregarJugadorButton;
+    private JButton vizualizarJugadoresButton;
+    private JButton buscarJugadorButton;
+    private JButton eliminarJugadorButton;
+}
